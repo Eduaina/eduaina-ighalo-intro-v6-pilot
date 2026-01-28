e54670d0a15adf44a668ff729529f41dfa321159
Eduaina Ighalo <eduaighalo29@gmail.com>
2026-01-28 11:54:25 +0100

    Updated the reset button and styled the entire form and message sections

--- js/index.js
+++ js/index.js
@@ -45,17 +45,21 @@ for (let i = 0; i < skills.length; i++) {
 //Handle Message Form Submit
 const messageForm = document.querySelector('form[name="leave_message"]');
 messageForm.addEventListener('submit', (e) => {
+  console.log('Form Submitted')
   e.preventDefault();
   let formName = e.target.usersName.value;
   let formEmail = e.target.usersEmail.value;
   let formMessage = e.target.usersMessage.value;
 
-  console.log(formName, formEmail, formMessage);
+  console.log(formName);
+  console.log(formEmail);
+  console.log(formMessage);
+
 
   const messageSection = document.getElementById('messages');
   const messageList = messageSection.querySelector('ul');
   const newMessage = document.createElement('li');
-  newMessage.innerHTML = `<a href="mailto: ${formEmail}">${formName}</a> <span>${formMessage}</span>`
+  newMessage.innerHTML = `<a href="mailto:${formEmail}">${formName}: </a> <span>${formMessage}</span>`
   console.log(newMessage);
 
   const removeButton = document.createElement('button');
@@ -67,8 +71,13 @@ messageForm.addEventListener('submit', (e) => {
   console.log(removeButton)
 
   removeButton.addEventListener('click', (event) => {
-    // const entry = 
+    const entry = event.target.parentNode;
+    entry.remove();
   })
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
 
   messageForm.reset();
 })
